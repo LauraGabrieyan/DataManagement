58de60431f9b2270434305ed8f3fb223931be226
LauraGabrielyan <lauragabrielyan35@gmail.com>
2025-04-05 21:36:33 +0400

    implemented typed httpClient for user and post APIs

--- DataManagement/Program.cs
+++ DataManagement/Program.cs
@@ -26,7 +26,7 @@ namespace DataManagement
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
